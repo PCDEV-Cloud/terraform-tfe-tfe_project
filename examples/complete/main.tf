27692adeb4ff5fce788481245f81cebfd1e2c4d9
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-08 17:07:13 +0100

    Update example with variable sets

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,10 +1,51 @@
 provider "tfe" {
-  token = "<TFE_TOKEN_HERE>"
+  token = "<TFE-TOKEN-HERE>"
+}
+
+resource "tfe_variable_set" "example" {
+  name         = "example-variable-set"
+  organization = "<TFE-ORGANIZATION-HERE>"
+  global       = false
 }
 
 module "tfe_project" {
   source = "../../"
 
-  organization = "my-organization"
-  name         = "example-project"
+  organization = "<TFE-ORGANIZATION-HERE>"
+  name         = "Example Project"
+
+  variable_sets = [
+    {
+      name = "Var-Set-Example-1"
+      variables = [
+        {
+          key   = "terraform-variable"
+          value = "value-of-terraform-variable"
+        },
+        {
+          key      = "ENV_VARIABLE"
+          value    = "value-of-env-variable"
+          category = "env"
+        }
+      ]
+    },
+    {
+      name = "Var-Set-Example-2"
+      variables = [
+        {
+          key   = "terraform-variable"
+          value = "value-of-terraform-variable"
+        },
+        {
+          key      = "ENV_VARIABLE"
+          value    = "value-of-env-variable"
+          category = "env"
+        }
+      ]
+    }
+  ]
+
+  additional_variable_sets = {
+    example = tfe_variable_set.example.id
+  }
 }
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,15 @@
 output "project_id" {
   value = module.tfe_project.id
 }
+
+output "name" {
+  value = module.tfe_project.name
+}
+
+output "variable_sets" {
+  value = module.tfe_project.variable_sets
+}
+
+output "project_variable_sets" {
+  value = module.tfe_project.project_variable_sets
+}
