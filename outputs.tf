95245c1c48fd982c3c2ff09be228184f4cdbf419
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-12-08 17:06:55 +0100

    Add variable sets and variables

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "id" {
   value       = tfe_project.this.id
   description = "The project ID."
 }
+
+output "name" {
+  value       = tfe_project.this.name
+  description = "Name of the project."
+}
+
+output "variable_sets" {
+  value       = [for i in tfe_variable_set.this : i]
+  description = "A list of variable sets with variables created by the module."
+}
+
+output "project_variable_sets" {
+  value       = concat([for i in tfe_project_variable_set.this : i], [for i in tfe_project_variable_set.additional : i])
+  description = "A list of variable sets applied to the project."
+}
